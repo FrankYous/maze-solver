144b6867670720303e57ce2ee05c10416d800758
FrankYous <f_s_yousefi@yahoo.com>
2023-03-11 21:44:13 +0330

    added draw line function

--- solver.py
+++ solver.py
@@ -1,7 +1,7 @@
 from tkinter import Tk, BOTH, Canvas
 
 class Window:
-    def __init__(self,width,height):
+    def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze")
         self.canvas = Canvas()
@@ -21,9 +21,35 @@ class Window:
     def close(self):
         self.running = False
 
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
+
+class Point:
+    def __init__(self, x_coord, y_coord):
+        self.x = x_coord
+        self.y = y_coord
+
+
+class Line:
+    def __init__(self, point1, point2):
+        self.x1 = point1.x
+        self.x2 = point2.x
+        self.y1 = point1.y
+        self.y2 = point2.y
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.x1, self.y1, self.x2, self.y2, fill=fill_color, width=2
+        )
+        canvas.pack()
 
 def main():
-    win = Window(800,600)
+    win = Window(800, 600)
+    po1 = Point(100, 100)
+    po2 = Point(200, 200)
+    line1 = Line(po1, po2)
+    win.draw_line(line1, "red")
     win.wait_for_close()
 
 
