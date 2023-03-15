05222840642fb6e8acd87e2873d96b3dd789fc02
FrankYous <f_s_yousefi@yahoo.com>
2023-03-15 17:46:33 +0330

    separate graphics from the main file

--- solver.py
+++ solver.py
@@ -1,50 +1,5 @@
-from tkinter import Tk, BOTH, Canvas
+from graphics import Window, Point, Line
 import time
-
-class Window:
-    def __init__(self, width, height):
-        self.__root = Tk()
-        self.__root.title("Maze")
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
-        self.canvas = Canvas(height = height, width = width)
-        self.canvas.pack()
-        self.running = False        
-
-    def redraw(self):
-        self.__root.update_idletasks()
-        self.__root.update()
-
-    def wait_for_close(self):
-        self.running = True
-        while self.running == True:
-            self.redraw()
-
-    def draw_line(self, line, fill_color):
-        line.draw(self.canvas, fill_color)
-
-    def close(self):
-        self.running = False
-
-
-class Point:
-    def __init__(self, x_coord, y_coord):
-        self.x = x_coord
-        self.y = y_coord
-
-
-class Line:
-    def __init__(self, point1, point2):
-        self.x1 = point1.x
-        self.x2 = point2.x
-        self.y1 = point1.y
-        self.y2 = point2.y
-
-    def draw(self, canvas, fill_color):
-        canvas.create_line(
-            self.x1, self.y1, self.x2, self.y2, fill=fill_color, width=2
-        )
-        canvas.pack()
-
 
 class Cell:
     def __init__(self, x1, y1, x2, y2, win):
